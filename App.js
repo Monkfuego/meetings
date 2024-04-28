7f43b4ef04a3bfd32a3a1be3ea32b8b4ec36f2d4
~ Lakshay <102456284+Monkfuego@users.noreply.github.com>
2024-04-28 19:50:50 +0530

    Update App.js

--- App.js
+++ App.js
@@ -9,18 +9,32 @@ import JoinMeeting from './JoinMeeting';
 import VideoCall from './VideoCall';
 
 // Firebase 
-const App = () => {
-  const [isInMeeting, setIsInMeeting] = useState(false);
-  const [meetingCode, setMeetingCode] = useState('');
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-analytics.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyA7xwKYfb4sIL8CdQu6RUZ4cIgx2BRgVvg",
+    authDomain: "meetings-57c01.firebaseapp.com",
+    projectId: "meetings-57c01",
+    storageBucket: "meetings-57c01.appspot.com",
+    messagingSenderId: "410327257218",
+    appId: "1:410327257218:web:c898669b43e7d57e34e49b",
+    measurementId: "G-6R2E3HGTB2"
+  };
 
-  const handleJoinMeeting = (code) => {
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const App = () => {
+const [isInMeeting, setIsInMeeting] = useState(false);
+const [meetingCode, setMeetingCode] = useState('');
+
+const handleJoinMeeting = (code) => {
     setMeetingCode(code);
     setIsInMeeting(true);
   };
+const user = firebase.auth().currentUser;
 
-  const user = firebase.auth().currentUser;
-
-  return (
+return (
     <div>
       <h1>MEETING APPLICATION</h1>
       {user ? (
