b38a9ad5e654e2bc430a0371fc4757e4bba5b9f6
~ Lakshay <102456284+Monkfuego@users.noreply.github.com>
2024-04-28 19:28:04 +0530

    Update VideoCall.js

--- VideoCall.js
+++ VideoCall.js
@@ -1,1 +1,29 @@
+import React, { useEffect, useRef } from 'react';
+import SimplePeer from 'simple-peer';
+
+const VideoCall = () => {
+  const videoRef = useRef();
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+      .then(stream => {
+        videoRef.current.srcObject = stream;
+
+        const peer = new SimplePeer({ initiator: true, stream });
+
+        peer.on('signal', data => {
+        });
+
+        peer.on('stream', stream => {
+        });
+      })
+      .catch(error => {
+        console.error('Error accessing media devices:', error);
+      });
+  }, []);
+
+  return <video ref={videoRef} autoPlay />;
+};
+
+export default VideoCall;
 
